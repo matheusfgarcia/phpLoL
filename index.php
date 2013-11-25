c79998c00176eb59a76808c831b9487e1e357e25
root <root@rarity53.vpshosting24.com>
2013-11-25 14:11:23 -0500

    new file:   index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+date_default_timezone_set("America/Los_Angeles");
+
+spl_autoload_register(
+    function($className)
+    {
+        $className = str_replace("_", "\\", $className);
+        $className = ltrim($className, '\\');
+        $fileName = '';
+        $namespace = '';
+        if ($lastNsPos = strripos($className, '\\'))
+        {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+        require $fileName;
+    }
+);
+
+require_once 'vendor/autoload.php';
+
+use zlokomatic\phpLoL\LoLClient;
+
+//use zlokomatic\phpLoL\examples\LOLProfiler;
+
+$client = new LoLClient('mfgsk', 'mafiga182', 'BR' );
+
+$summonerName = $_GET['user'];
+
+$summoner = $client->getSummonerByName($summonerName);
+
+$publicSummonerData = $client->getAllPublicSummonerDataByAccount($summoner->getAcctId());
+
+$names = $client->getSummonerNames(array($summoner->getAcctId(), 123456));
+
+$games = $client->getRecentGames($summoner->getAcctId());
+
+$stats = $client->getPlayerStatsByAccountId($summoner->getAcctId());
+
+$aggStats = $client->getAggregatedStats($summoner->getAcctId(), 'CLASSIC', 3);
+
+$leagues = $client->getAllLeaguesForPlayer($summoner->getSummonerId());
+
+// 20-10
+$masteries = $client->getMasteryBook($summoner->getSummonerId());
+
+/*
+
+$profiler = new LOLProfiler('Username','Password','NA');
+
+$profiler->searchSummoner('SummonerName');
+
+echo $profiler->name;
+
+*/
+
+print_r($stats);
+
